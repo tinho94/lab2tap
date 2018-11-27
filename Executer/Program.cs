6d7e26552194b81343c9789a4fa17ab3fc5a1a70
Tinho <4127466@studenti.unige.it>
2018-11-27 12:10:06 +0100

    riga 64 MyLibrary: aggiunta classe ExtraClass con costruttore non di default, Un Eccezione nel Main di Program.cs viene lanciata dal metodo CreateInstance alla riga 34 poichè non vengono gestiti casi con classi che abbiano costruttore diverso da quello di default

--- Executer/Program.cs
+++ Executer/Program.cs
@@ -12,20 +12,6 @@ namespace Executer
         {
             var a = Assembly.LoadFrom("../../../MyLibrary/bin/Debug/MyLibrary.dll");
            
-            /*
-            Aggiungete al Main del codice che, sfruttando la reflection, invochi tutti i metodi pubblici 
-            (di tutte le classi trovate nella DLL) che siano stati annotati con [ExecuteMe], 
-            passando come argomenti gli argomenti dell'annotazione.
-            Facendo riferimento alla classe Foo mostrata sopra, M1 dovrà essere invocato senza parametri, 
-            il metodo M2 dovrà essere invocato tre volte 
-            (con argomenti: 3, 0 e 45) e M3 dovrà essere invocato una volta con argomenti s1="hello" e s2="reflection".
-            Notate che per invocare i metodi d'istanza dovrete prima creare degli oggetti 
-            (motivo per cui abbiamo detto di inserire classi con costruttori pubblici senza argomenti)...Hint: classe Activator...
-            In questa prima release assumete che non ci siano errori nelle annotazioni 
-            ovvero che numero e tipi degli argomenti di ciascuna annotazione corrispondano 
-            a quanto necessario per l'invocazione del metodo annotato.
-            Assumete anche che i parametri dei metodi in MyLibrary siano tutti per valore, necessari e non "params".
-            */
             foreach (Type _class in a.GetTypes())
             {
                 if (_class.IsClass)
